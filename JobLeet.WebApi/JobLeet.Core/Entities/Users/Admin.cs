e4cec3e2cf6adc0437ca93f88bb61089651a6348
Nishant Banjade <nishantbanjade07@gmail.com>
2023-11-24 22:13:06 +0530

    feat: add Entity class for different Entities

---
+++ JobLeet.WebApi/JobLeet.Core/Entities/Users/Admin.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+namespace JobLeet.WebApi.JobLeet.Core.Entities.Users
+{
+    public class Admin : BaseEntity
+    {
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+    }
+}
