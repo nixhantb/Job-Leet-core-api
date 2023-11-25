00ab42ef966f69ee15b9d31c8e77327ade6d9a4b
Nishant Banjade <nishantbanjade07@gmail.com>
2023-11-25 16:12:47 +0530

    feat: add entity for Experience

--- JobLeet.WebApi/JobLeet.Core/Entities/Common/V1/Proficiencies/Experience.cs
+++ JobLeet.WebApi/JobLeet.Core/Entities/Common/V1/Proficiencies/Experience.cs
@@ -1,6 +1,17 @@
 ﻿namespace JobLeet.WebApi.JobLeet.Core.Entities.Common.V1.Proficiencies
 {
-    public class Experience
+    public class Experience : BaseEntity
     {
+        public ExperienceLevel ExperienceLevel { get; set; }
+    }
+    public enum ExperienceLevel
+    {
+        EntryLevel,
+        Intermediate,
+        Senior,
+        Lead,
+        Principal,
+        Executive,
+        Others
     }
 }
