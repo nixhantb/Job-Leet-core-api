2be9074ca119578866d870aabaf84022aa683691
Nishant Banjade <nishantbanjade07@gmail.com>
2023-11-25 13:58:46 +0530

    feat: add Employers Entity defination and structure

---
+++ JobLeet.WebApi/JobLeet.Core/Entities/Employers/V1/Employer.cs
@@ -0,0 +1,40 @@
+﻿using JobLeet.WebApi.JobLeet.Core.Entities.Accounts.V1.Users;
+using JobLeet.WebApi.JobLeet.Core.Entities.Common.V1.Shared;
+using JobLeet.WebApi.JobLeet.Core.Entities.Companies.V1.IndustryTypes;
+using JobLeet.WebApi.JobLeet.Core.Entities.Companies.V1.Profiles;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JobLeet.WebApi.JobLeet.Core.Entities.Employers.V1
+{
+    public class Employer : BaseEntity
+    {
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public User User { get; set; }
+        [ForeignKey("Name")]
+        public int NameId { get; set; }
+        public PersonName Name { get; set; }
+        [ForeignKey("Address")]
+        public int AddressId { get; set; }
+        public Address Address { get; set; }
+
+        [ForeignKey("Phone")]
+        public int PhoneId { get; set; }
+        public Phone Phone { get; set; }
+
+        [ForeignKey("Profile")]
+        public int ProfileId { get; set; }
+        public CompanyProfile Profile { get; set; }
+        public EmployerType EmployerType { get; set; }
+        [ForeignKey("IndustryType")]
+        public int IndustryTypeId { get; set; }
+        public IndustryType IndustryType { get; set; }
+
+    }
+    public enum EmployerType
+    {
+        SmallBusiness,
+        MediumBusiness,
+        LargeCorporation
+    }
+}
