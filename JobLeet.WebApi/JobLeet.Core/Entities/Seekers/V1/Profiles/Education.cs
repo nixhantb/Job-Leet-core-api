4918890b2b06035bc880fb581655ed32f38bb744
Nishant Banjade <nishantbanjade07@gmail.com>
2023-11-25 09:15:45 +0530

    feat: Add Education Entity for Seekers

---
+++ JobLeet.WebApi/JobLeet.Core/Entities/Seekers/V1/Profiles/Education.cs
@@ -0,0 +1,12 @@
+﻿namespace JobLeet.WebApi.JobLeet.Core.Entities.Seekers.V1.Profiles
+{
+    public class Education
+    {
+        public string Degree { get; set; }
+        public string Major { get; set; }
+        public string Institution { get; set; }
+        public DateOnly GraduationDate {  get; set; }
+        public decimal Cgpa {  get; set; }
+
+    }
+}
