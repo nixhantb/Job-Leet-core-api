52e7d8b64586a9e36e92587e171d470b94bc7c76
Nishant Banjade <nishantbanjade07@gmail.com>
2023-11-25 13:48:01 +0530

    feat: add Companies Entity for Company related information

---
+++ JobLeet.WebApi/JobLeet.Core/Entities/Companies/V1/IndustryTypes/IndustryType.cs
@@ -0,0 +1,15 @@
+﻿namespace JobLeet.WebApi.JobLeet.Core.Entities.Companies.V1.IndustryTypes
+{
+    public class IndustryType : BaseEntity
+    {
+        public IndustryCategory IndustryCategory {  get; set; }
+    }
+    public enum IndustryCategory
+    {
+        Technology,
+        Healthcare,
+        Finance,
+        Manufacturing,
+        Others
+    }
+}
---
+++ JobLeet.WebApi/JobLeet.Core/Entities/Companies/V1/Profiles/CompanyProfile.cs
@@ -0,0 +1,26 @@
+﻿using JobLeet.WebApi.JobLeet.Core.Entities.Common.V1.Shared;
+using JobLeet.WebApi.JobLeet.Core.Entities.Companies.V1.IndustryTypes;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JobLeet.WebApi.JobLeet.Core.Entities.Companies.V1.Profiles
+{
+    public class CompanyProfile : BaseEntity
+    {
+        public string ProfileInfo { get; set; }
+        [ForeignKey("Address")]
+        public Address CompanyAddress {  get; set; }
+        [ForeignKey("Phone")]
+        public Phone ContactPhone {  get; set; }
+        [ForeignKey("Email")]
+        public Email ContactEmail {  get; set; }
+        /// <summary>
+        /// Website can be extended in future based on the requirements
+        /// </summary>
+        public string Website {  get; set; }
+        [ForeignKey("IndustryType")]
+        public IndustryType IndustryType { get; set; }
+
+
+
+    }
+}
