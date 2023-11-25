c408ac2c3ec7fa5a62aadc9202c6d197a98e903a
Nishant Banjade <nishantbanjade07@gmail.com>
2023-11-25 16:30:23 +0530

    feat: add Entities for JobLeet . Closes Issue #15

--- JobLeet.WebApi/JobLeet.Core/Entities/Profiles/CompanyProfile.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace JobLeet.WebApi.JobLeet.Core.Entities.Profiles
-{
-    public class CompanyProfile : BaseEntity
-    {
-        public string ProfileInfo { get; set; }
-    }
-}
--- JobLeet.WebApi/JobLeet.Core/Entities/Users/Admin.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-namespace JobLeet.WebApi.JobLeet.Core.Entities.Users
-{
-    public class Admin : BaseEntity
-    {
-        [ForeignKey("User")]
-        public int UserId { get; set; }
-    }
-}
