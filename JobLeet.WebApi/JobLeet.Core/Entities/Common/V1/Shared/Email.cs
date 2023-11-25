18d88db1a30362ff43f3cc0473cf82b1b17d3623
Nishant Banjade <nishantbanjade07@gmail.com>
2023-11-25 13:30:50 +0530

    chore: update Email.cs into Common Shared Entities

---
+++ JobLeet.WebApi/JobLeet.Core/Entities/Common/V1/Shared/Email.cs
@@ -0,0 +1,14 @@
+﻿namespace JobLeet.WebApi.JobLeet.Core.Entities.Common.V1.Shared
+{
+    public class Email : BaseEntity
+    {
+        public EmailType EmailType { get; set; }
+        public string EmailAddress { get; set; }
+    }
+    public enum EmailType
+    {
+        Personal,
+        Work,
+        Other
+    }
+}
