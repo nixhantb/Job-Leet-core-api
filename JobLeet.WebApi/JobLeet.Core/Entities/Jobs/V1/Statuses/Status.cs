74da1a0b61aa0a0a289d78dfd71705845f06705a
Nishant Banjade <nishantbanjade07@gmail.com>
2023-11-25 08:08:26 +0530

    feat: Add version1 Entity for Statuses which maintains good seperation of concerns and it can easily be extended in future

---
+++ JobLeet.WebApi/JobLeet.Core/Entities/Jobs/V1/Statuses/Status.cs
@@ -0,0 +1,15 @@
+﻿namespace JobLeet.WebApi.JobLeet.Core.Entities.Jobs.V1.Statuses
+{
+    public class Status : BaseEntity
+    {
+        public StatusName StatusName { get; set; }
+    }
+    public enum StatusName
+    {
+        Active, 
+        Inactive,
+        Pending,
+        Completed,
+        Cancelled
+    }
+}
