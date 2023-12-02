8f65a3f5a195b2a358550fcb8d83b40ba5344ee6
Nishant Banjade <nishantbanjade07@gmail.com>
2023-12-02 23:17:21 +0530

    add employer model (dto) schema

---
+++ JobLeet.WebApi/JobLeet.Api/Models/Common/V1/PersonNameModel.cs
@@ -0,0 +1,9 @@
+﻿namespace JobLeet.WebApi.JobLeet.Api.Models.Common.V1
+{
+    public class PersonNameModel : BaseModel
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ JobLeet.WebApi/JobLeet.Api/Models/Employers/V1/EmployerModel.cs
@@ -0,0 +1,22 @@
+﻿using JobLeet.WebApi.JobLeet.Api.Models.Common.V1;
+using JobLeet.WebApi.JobLeet.Api.Models.Companies.V1;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JobLeet.WebApi.JobLeet.Api.Models.Employers.V1
+{
+    public class EmployerModel : BaseModel
+    {
+        public PersonNameModel Name { get; set; }
+        public AddressModel Address { get; set; }
+        public PhoneModel Phone { get; set; }
+        public CompanyProfileModel Profile { get; set; }
+        public EmployerTypeModel EmployerType { get; set; }
+        public IndustryTypeModel IndustryType { get; set; }
+    }
+    public enum EmployerTypeModel
+    {
+        SmallBusiness,
+        MediumBusiness,
+        LargeCorporation
+    }
+}
