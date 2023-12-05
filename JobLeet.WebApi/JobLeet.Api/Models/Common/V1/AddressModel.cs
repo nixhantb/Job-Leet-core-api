614c7df80f655983c22aea812f826c9c0bbe42c8
Nishant Banjade <nishantbanjade07@gmail.com>
2023-12-05 21:44:08 +0530

    feat: add validations and data annotations in models

--- JobLeet.WebApi/JobLeet.Api/Models/Common/V1/AddressModel.cs
+++ JobLeet.WebApi/JobLeet.Api/Models/Common/V1/AddressModel.cs
@@ -1,11 +1,17 @@
-﻿namespace JobLeet.WebApi.JobLeet.Api.Models.Common.V1
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobLeet.WebApi.JobLeet.Api.Models.Common.V1
 {
     public class AddressModel : BaseModel
     {
+
         public string Street { get; set; }
         public string City { get; set; }
         public string State { get; set; }
+        [Required(ErrorMessage = "Postal Code is required")]
+        [RegularExpression(@"^\d{5}(-\d{4})?$", ErrorMessage = "Invalid Postal Code format")]
         public string PostalCode { get; set; }
+        [Required(ErrorMessage = "Country is required")]
         public string Country { get; set; }
     }
 }
--- JobLeet.WebApi/JobLeet.Api/Models/Companies/V1/CompanyProfileModel.cs
+++ JobLeet.WebApi/JobLeet.Api/Models/Companies/V1/CompanyProfileModel.cs
@@ -1,22 +1,26 @@
 ﻿
 using JobLeet.WebApi.JobLeet.Api.Models.Common.V1;
+using System.ComponentModel.DataAnnotations;
 
 namespace JobLeet.WebApi.JobLeet.Api.Models.Companies.V1
 {
     public class CompanyProfileModel : BaseModel
     {
+        [Required(ErrorMessage = "Profile Info is required")]
+        [StringLength(1000)]
         public string ProfileInfo { get; set; }
-       
+        [Required(ErrorMessage = "Company address is required")]
         public AddressModel CompanyAddress { get; set; }
-        
+        [Required(ErrorMessage = "Phone detail is required")]
         public PhoneModel ContactPhone { get; set; }
-       
+        [Required(ErrorMessage = "Email detail is required")]
         public EmailModel ContactEmail { get; set; }
         /// <summary>
         /// Website can be extended in future based on the requirements
         /// </summary>
+        [Required(ErrorMessage = "Website information is required")]
         public string Website { get; set; }
-       
+        [Required(ErrorMessage = "Industry type is required")]
         public IndustryTypeModel IndustryType { get; set; }
     }
 }
