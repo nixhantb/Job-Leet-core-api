bf64f28ef7a197bc934c0f0e0ef81ca53e3a6a4e
Nishant Banjade <nishantbanjade07@gmail.com>
2023-12-09 22:03:24 +0530

    test: add tests for PhoneModel

--- JobLeet.WebApi/JobLeet.Api/Models/Common/V1/PhoneModel.cs
+++ JobLeet.WebApi/JobLeet.Api/Models/Common/V1/PhoneModel.cs
@@ -5,7 +5,7 @@ namespace JobLeet.WebApi.JobLeet.Api.Models.Common.V1
     public class PhoneModel : BaseModel
     {
         [Required(ErrorMessage = "Country Code is required")]
-        [Range(1, int.MaxValue, ErrorMessage = "Country Code must be a positive number")]
+        [Range(0, int.MaxValue, ErrorMessage = "Country Code must be a positive number")]
         public int CountryCode { get; set; }
         [Required(ErrorMessage = "Phone number is required")]
         [RegularExpression(@"^[0-9]+$", ErrorMessage = "Invalid Phone Number format. Use only digits.")]
