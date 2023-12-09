1bacf7afe38ed171edaf0ecbe1436204705a0c5f
Nishant Banjade <nishantbanjade07@gmail.com>
2023-12-09 21:19:55 +0530

    test: add unit tests for Companies models

---
+++ JobLeet.Tests/UnitTests/JobLeet.Api.Tests/Models/Companies/V1/IndustryTypeModelTest.cs
@@ -0,0 +1,21 @@
+﻿
+
+using JobLeet.WebApi.JobLeet.Api.Models.Companies.V1;
+
+namespace UnitTests.JobLeet.Api.Tests.Models.Companies.V1
+{
+    public class IndustryTypeModelTest
+    {
+        [Fact]
+        public void IndustryTypeModel_ShouldInstantiateSuccessfully()
+        {
+            IndustryTypeModel model = new IndustryTypeModel
+            {
+                IndustryCategory = IndustryCategory.Technology,
+            };
+
+            Assert.Equal(IndustryCategory.Technology, model.IndustryCategory);
+        }
+
+    }
+}
