d2a7972ecf34ad1b85cf2a6e9ed94f5496dd69d0
Nishant Banjade <nishantbanjade07@gmail.com>
2023-12-10 11:41:26 +0530

    feat: add UserRegisterRequest class with validations | Annotations

--- JobLeet.WebApi/JobLeet.Api/Models/Accounts/V1/UserRegisterRequest.cs
+++ JobLeet.WebApi/JobLeet.Api/Models/Accounts/V1/UserRegisterRequest.cs
@@ -1,6 +1,30 @@
-﻿namespace JobLeet.WebApi.JobLeet.Api.Models.Accounts.V1
+﻿using JobLeet.WebApi.JobLeet.Api.Models.Common.V1;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace JobLeet.WebApi.JobLeet.Api.Models.Accounts.V1
 {
-    public class UserRegisterRequest
+    public class UserRegisterRequest : BaseModel
     {
+        [Required(ErrorMessage = "Person Name is required")]
+        public PersonNameModel PersonName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address Format")]
+        public EmailModel Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{8,}$",
+            ErrorMessage = "Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character.")]
+        public string Password {  get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [DataType(DataType.Password)]
+        [MinLength(8, ErrorMessage = "Confirm Password must be at least 8 characters")]
+        [Compare(nameof(Password), ErrorMessage = "Password do not match")]
+        public string ConfirmPassword { get; set; }
+ 
     }
 }
