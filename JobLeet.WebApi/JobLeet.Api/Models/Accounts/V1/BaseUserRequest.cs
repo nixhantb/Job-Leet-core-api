febeaa77268b01ad3eae9f60c43aceca2d57de3e
Nishant Banjade <nishantbanjade07@gmail.com>
2023-12-10 12:03:56 +0530

    feat: add Accounts model with validations and BaseUserRequest Model

---
+++ JobLeet.WebApi/JobLeet.Api/Models/Accounts/V1/BaseUserRequest.cs
@@ -0,0 +1,19 @@
+﻿using JobLeet.WebApi.JobLeet.Api.Models.Common.V1;
+using System.ComponentModel.DataAnnotations;
+
+namespace JobLeet.WebApi.JobLeet.Api.Models.Accounts.V1
+{
+    public class BaseUserRequest : BaseModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address Format")]
+        public EmailModel Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{8,}$",
+            ErrorMessage = "Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character.")]
+        public string Password { get; set; }
+    }
+}
--- JobLeet.WebApi/JobLeet.Api/Models/Accounts/V1/UserRegisterRequest.cs
+++ JobLeet.WebApi/JobLeet.Api/Models/Accounts/V1/UserRegisterRequest.cs
@@ -4,21 +4,10 @@ using System.ComponentModel.DataAnnotations;
 
 namespace JobLeet.WebApi.JobLeet.Api.Models.Accounts.V1
 {
-    public class UserRegisterRequest : BaseModel
+    public class UserRegisterRequest : BaseUserRequest
     {
         [Required(ErrorMessage = "Person Name is required")]
         public PersonNameModel PersonName { get; set; }
-
-        [Required(ErrorMessage = "Email is required")]
-        [EmailAddress(ErrorMessage = "Invalid Email Address Format")]
-        public EmailModel Email { get; set; }
-
-        [Required(ErrorMessage = "Password is required")]
-        [DataType(DataType.Password)]
-        [MinLength(8, ErrorMessage = "Password must be at least 8 characters")]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{8,}$",
-            ErrorMessage = "Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character.")]
-        public string Password {  get; set; }
 
         [Required(ErrorMessage = "Confirm Password is required")]
         [DataType(DataType.Password)]
