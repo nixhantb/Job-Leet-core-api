d178a43fa05d9a3b3ab49f2c43fa6a340c8211e7
Nishant Banjade <nishantbanjade07@gmail.com>
2023-12-11 22:14:46 +0530

    chore: add validations for EducationModel

--- JobLeet.WebApi/JobLeet.Api/Models/Common/V1/EducationModel.cs
+++ JobLeet.WebApi/JobLeet.Api/Models/Common/V1/EducationModel.cs
@@ -1,11 +1,19 @@
-﻿namespace JobLeet.WebApi.JobLeet.Api.Models.Common.V1
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobLeet.WebApi.JobLeet.Api.Models.Common.V1
 {
     public class EducationModel : BaseModel
     {
+        [Required(ErrorMessage = "Degree is required")]
         public string Degree { get; set; }
+        [Required(ErrorMessage = "Major is required")]
         public string Major { get; set; }
+        [Required(ErrorMessage = "Institution is required")]
         public string Institution { get; set; }
+        [Required(ErrorMessage = "Graduation Date is required")]
+        [DataType(DataType.Date)]
         public DateOnly GraduationDate { get; set; }
+        [Range(0, 4.0, ErrorMessage = "CGPA must be between 0.0 and 4.0")]
         public decimal Cgpa { get; set; }
     }
 }
