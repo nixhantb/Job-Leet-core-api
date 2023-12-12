ff9553899c9d5df9f56c6c79673f293591b76656
Nishant Banjade <nishantbanjade07@gmail.com>
2023-12-12 18:18:27 +0530

    add: model and test for skillsModel

---
+++ JobLeet.Tests/UnitTests/JobLeet.Api.Tests/Models/Common/V1/SkillModelTest.cs
@@ -0,0 +1,22 @@
+﻿using JobLeet.WebApi.JobLeet.Api.Models.Common.V1;
+using System.ComponentModel.DataAnnotations;
+
+namespace UnitTests.JobLeet.Api.Tests.Models.Common.V1
+{
+    public class SkillModelTest
+    {
+        [Fact]
+        public void SkillModel_Validation_Success()
+        {
+            // Arrange
+            var skillModel = new SkillModel
+            {
+                Title = "Programming",
+                Description = "Excellent coding skills."
+            };
+
+            
+            Assert.NotNull(skillModel);
+        }
+    }
+}
--- JobLeet.WebApi/JobLeet.Api/Models/Common/V1/SkillModel.cs
+++ JobLeet.WebApi/JobLeet.Api/Models/Common/V1/SkillModel.cs
@@ -1,6 +1,8 @@
 ﻿namespace JobLeet.WebApi.JobLeet.Api.Models.Common.V1
 {
-    public class SkillModel
+    public class SkillModel : BaseModel
     {
+        public string Title { get; set; }
+        public string Description { get; set; }
     }
 }
