709254e2c4f76ab571a0bbbb8145c67dbe7bdd82
Nishant Banjade <nishantbanjade07@gmail.com>
2023-12-12 18:29:15 +0530

    add unit test for JobModel

---
+++ JobLeet.Tests/UnitTests/JobLeet.Api.Tests/Models/Jobs/V1/JobModelTest.cs
@@ -0,0 +1,26 @@
+﻿
+using JobLeet.WebApi.JobLeet.Api.Models.Jobs.V1;
+
+namespace UnitTests.JobLeet.Api.Tests.Models.Jobs.V1
+{
+    public class JobModelTest
+    {
+        [Fact]
+        public void JobModel_ShouldInstantiateSuccessfully()
+        {
+            JobModel jobModel = new JobModel
+            {
+
+                JobTitle = "SDE",
+                JobDescription = "Description",
+                Vacancies = 1,
+                BasicPay = 1234.66m,
+                FunctionalArea = "CA",
+                PostingDate = new DateTime(),
+
+            };
+
+            Assert.NotNull(jobModel);
+        }
+    }
+}
