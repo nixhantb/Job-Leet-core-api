72c7a34468b3103002ee1446d4bf95ce3f83d109
Nishant Banjade <nishantbanjade07@gmail.com>
2023-12-15 09:05:29 +0530

    feat: add interface for IEmailModelType that supports GetByID and GETALL method

---
+++ JobLeet.WebApi/JobLeet.Core/Interfaces/Common/V1/IEmailModelType.cs
@@ -0,0 +1,11 @@
+﻿using JobLeet.WebApi.JobLeet.Core.Entities.Common.V1;
+
+namespace JobLeet.WebApi.JobLeet.Core.Interfaces.Common.V1
+{
+    public interface IEmailModelType : IRepository<EmailType>
+    {
+        Task<EmailType> GetByIdAsync(int id);
+        Task<IEnumerable<EmailType>> GetAllAsync();
+       
+    }
+}
