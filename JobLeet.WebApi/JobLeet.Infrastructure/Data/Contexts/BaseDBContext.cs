977adb90e5eecb9ae13eb675b47cb4a129fd455a
Nishant Banjade <nishantbanjade07@gmail.com>
2023-12-17 17:57:08 +0530

    feat: update EmailType api end point exposing only EmailTypes from EmailModel

--- JobLeet.WebApi/JobLeet.Infrastructure/Data/Contexts/BaseDBContext.cs
+++ JobLeet.WebApi/JobLeet.Infrastructure/Data/Contexts/BaseDBContext.cs
@@ -1,5 +1,5 @@
-﻿using JobLeet.WebApi.JobLeet.Core.Entities;
-using JobLeet.WebApi.JobLeet.Core.Entities.Common.V1;
+﻿using JobLeet.WebApi.JobLeet.Core.Entities.Common.V1;
+using JobLeet.WebApi.JobLeet.Infrastructure.Data.Contexts.V1;
 using Microsoft.EntityFrameworkCore;
 namespace JobLeet.WebApi.JobLeet.Infrastructure.Data.Contexts
 {
@@ -8,26 +8,10 @@ namespace JobLeet.WebApi.JobLeet.Infrastructure.Data.Contexts
         public BaseDBContext(DbContextOptions<BaseDBContext> options) : base(options)
         {
         }
-
         public virtual DbSet<Email> Emails { get; set; }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Email>(e =>
-            {
-                e.ToTable("Email");
-                e.HasKey(e => e.Id);
-                e.Property(e => e.Id).HasColumnName("email_id");
-                e.Property(e => e.EmailType).HasColumnName("email_type");
-                e.Property(e => e.EmailAddress).HasColumnName("email_address");
-                e.OwnsOne(email => email.MetaData);
-            });
-
-            // Configure MetaData as owned by BaseEntity
-            modelBuilder.Entity<BaseEntity>().OwnsOne(b => b.MetaData);
-
-            // Seed data for Email with MetaDat
-
+            modelBuilder.ApplyConfiguration(new EmailConfiguration());
             base.OnModelCreating(modelBuilder);
         }
     }
