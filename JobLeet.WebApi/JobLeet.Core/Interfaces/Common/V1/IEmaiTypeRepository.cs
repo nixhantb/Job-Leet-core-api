3d9729641e2674ca014a98f4327d8288bcf25d7f
Nishant Banjade <nishantbanjade07@gmail.com>
2023-12-17 10:17:26 +0530

    add email-types support for the endpoint: beta

--- JobLeet.WebApi/JobLeet.Core/Interfaces/Common/V1/IEmaiTypeRepository.cs
+++ JobLeet.WebApi/JobLeet.Core/Interfaces/Common/V1/IEmaiTypeRepository.cs
@@ -1,8 +1,7 @@
-﻿using JobLeet.WebApi.JobLeet.Core.Entities.Common.V1;
-
+﻿using JobLeet.WebApi.JobLeet.Api.Models.Common.V1;
 namespace JobLeet.WebApi.JobLeet.Core.Interfaces.Common.V1
 {
-    public interface IEmaiTypeRepository : IRepository<EmailType>
+    public interface IEmaiTypeRepository : IRepository<EmailModel>
     {
         
        
--- JobLeet.WebApi/JobLeet.Core/Interfaces/IRepository.cs
+++ JobLeet.WebApi/JobLeet.Core/Interfaces/IRepository.cs
@@ -7,7 +7,7 @@
     public interface IRepository<T> where T : class
     {
         Task<T> GetByIdAsync(int id);
-        Task<IEnumerable<T>> GetAllAsync();
+        Task<List<T>> GetAllAsync();
         Task AddAsync(T entity);
         Task UpdateAsync(T entity);
         Task DeleteAsync(int id);
