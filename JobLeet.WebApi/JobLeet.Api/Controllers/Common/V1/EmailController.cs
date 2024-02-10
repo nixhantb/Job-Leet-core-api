badb681142d213300f36a1f480561cda9bc6260d
Nishant Banjade <nishantbanjade07@gmail.com>
2024-02-10 09:17:41 +0545

    chore: fix email-types end point resource name

--- JobLeet.WebApi/JobLeet.Api/Controllers/Common/V1/EmailController.cs
+++ JobLeet.WebApi/JobLeet.Api/Controllers/Common/V1/EmailController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace JobLeet.WebApi.JobLeet.Api.Controllers.Common.V1
 {
-    [Route("api/v1/emailtypes")]
+    [Route("api/v1/email-types")]
     
     public class EmailController : BaseApiController<EmailType, IEmaiTypeRepository>
     {
