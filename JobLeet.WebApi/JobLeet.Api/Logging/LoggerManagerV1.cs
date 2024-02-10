72e600806f597cb473969cf8143f32744e72a3ce
Nishant Banjade <nishantbanjade07@gmail.com>
2024-02-10 10:36:35 +0545

    add: implement the loggerManager

---
+++ JobLeet.WebApi/JobLeet.Api/Logging/LoggerManagerV1.cs
@@ -0,0 +1,28 @@
+﻿using NLog;
+namespace JobLeet.WebApi.JobLeet.Api.Logging
+{
+    public class LoggerManagerV1 : ILoggerManagerV1
+    {
+        private static NLog.ILogger _logger = LogManager.GetCurrentClassLogger();
+
+        public void LogDebug(string message)
+        {
+            _logger.Debug(message);
+        }
+
+        public void LogError(string message)
+        {
+            _logger.Error(message);
+        }
+
+        public void LogInfo(string message)
+        {
+            _logger.Info(message);
+        }
+
+        public void LogWarn(string message)
+        {
+            _logger.Warn(message);
+        }
+    }
+}
