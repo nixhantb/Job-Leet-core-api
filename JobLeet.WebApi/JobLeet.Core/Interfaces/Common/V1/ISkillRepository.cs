d17b70c47fe22b9e9cf993153037422b3b75f657
Nishant Banjade <nishantbanjade07@gmail.com>
2024-02-14 23:28:34 +0545

    Add repository interface for Skill

---
+++ JobLeet.WebApi/JobLeet.Core/Interfaces/Common/V1/ISkillRepository.cs
@@ -0,0 +1,8 @@
+﻿using JobLeet.WebApi.JobLeet.Api.Models.Common.V1;
+
+namespace JobLeet.WebApi.JobLeet.Core.Interfaces.Common.V1
+{
+    public interface ISkillRepository : IRepository<SkillModel>
+    {
+    }
+}
