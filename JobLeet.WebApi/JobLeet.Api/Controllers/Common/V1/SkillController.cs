b2454ce4765612fcac87db085d54744f597233de
Nishant Banjade <nishantbanjade07@gmail.com>
2024-02-15 13:39:41 +0545

    add: resources name

--- JobLeet.WebApi/JobLeet.Api/Controllers/Common/V1/SkillController.cs
+++ JobLeet.WebApi/JobLeet.Api/Controllers/Common/V1/SkillController.cs
@@ -1,8 +1,7 @@
 ﻿using JobLeet.WebApi.JobLeet.Api.Logging;
 using JobLeet.WebApi.JobLeet.Api.Models.Common.V1;
 using JobLeet.WebApi.JobLeet.Core.Interfaces.Common.V1;
-using Microsoft.AspNetCore.Components;
-
+using Microsoft.AspNetCore.Mvc;
 namespace JobLeet.WebApi.JobLeet.Api.Controllers.Common.V1
 {
     [Route("api/v1/skills")]
