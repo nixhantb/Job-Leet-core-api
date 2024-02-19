02270b698478f3f578c32bc5280650ec93e6c1a0
Nishant Banjade <nishantbanjade07@gmail.com>
2024-02-19 21:21:30 +0545

    add personName controller , extending BaseApiController

--- JobLeet.WebApi/JobLeet.Api/Controllers/Common/V1/PersonNameController.cs
+++ JobLeet.WebApi/JobLeet.Api/Controllers/Common/V1/PersonNameController.cs
@@ -1,9 +1,12 @@
 ﻿using JobLeet.WebApi.JobLeet.Api.Logging;
 using JobLeet.WebApi.JobLeet.Api.Models.Common.V1;
 using JobLeet.WebApi.JobLeet.Core.Interfaces.Common.V1;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JobLeet.WebApi.JobLeet.Api.Controllers.Common.V1
 {
+    [Route("api/v1/person-names")]
+
     public class PersonNameController: BaseApiController<PersonNameModel, IPersonNameRepository>
     {
         public PersonNameController(IPersonNameRepository personNameRepository, ILoggerManagerV1 logger)
