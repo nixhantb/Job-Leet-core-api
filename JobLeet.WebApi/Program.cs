aa3129a3c946e3a735a5cc31d368daaad29eb8e8
Nishant Banjade <nishantbanjade07@gmail.com>
2024-02-19 21:21:50 +0545

    update scoped for PersonNameInterface

--- JobLeet.WebApi/Program.cs
+++ JobLeet.WebApi/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddScoped<DbContext, BaseDBContext>();
 builder.Services.AddScoped<IEmaiTypeRepository, EmailTypeRepository>();
 builder.Services.AddSingleton<ILoggerManagerV1, LoggerManagerV1>(); 
 builder.Services.AddScoped<ISkillRepository, SkillRepository>();
+builder.Services.AddScoped<IPersonNameRepository, PersonNameRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
