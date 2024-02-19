ff07df537c4120372a507e56cb2dc2613c649e94
Nishant Banjade <nishantbanjade07@gmail.com>
2024-02-19 21:20:48 +0545

    add PersonNameRepository

--- JobLeet.WebApi/JobLeet.Infrastructure/Repositories/Common/V1/PersonNameRepository.cs
+++ JobLeet.WebApi/JobLeet.Infrastructure/Repositories/Common/V1/PersonNameRepository.cs
@@ -1,15 +1,40 @@
 ﻿using JobLeet.WebApi.JobLeet.Api.Models.Common.V1;
 using JobLeet.WebApi.JobLeet.Core.Interfaces.Common.V1;
+using JobLeet.WebApi.JobLeet.Infrastructure.Data.Contexts;
 using Microsoft.EntityFrameworkCore;
 
 namespace JobLeet.WebApi.JobLeet.Infrastructure.Repositories.Common.V1
 {
     public class PersonNameRepository : IPersonNameRepository
     {
-        private readonly DbContext _dbContext;
-        public PersonNameRepository(DbContext dbContext)
+        private readonly BaseDBContext _dbContext;
+        public PersonNameRepository(BaseDBContext dbContext)
         {
             _dbContext = dbContext;
+        }
+        public async Task<List<PersonNameModel>> GetAllAsync()
+        {
+            try
+            {
+                var result = await _dbContext.PersonNames
+                     .Select(e => new PersonNameModel
+                     {
+                       Id = e.Id,
+                       FirstName = e.FirstName,
+                       MiddleName = e.MiddleName,
+                       LastName = e.LastName
+                     }).ToListAsync();
+                return result;
+            }
+            catch(DbUpdateException ex)
+            {
+                throw new Exception("Error while updating the database. Please try again later." + ex.Message);
+            }
+        }
+
+        public Task<PersonNameModel> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
         }
 
         public Task AddAsync(PersonNameModel entity)
@@ -18,16 +43,6 @@ namespace JobLeet.WebApi.JobLeet.Infrastructure.Repositories.Common.V1
         }
 
         public Task DeleteAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<List<PersonNameModel>> GetAllAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<PersonNameModel> GetByIdAsync(int id)
         {
             throw new NotImplementedException();
         }
