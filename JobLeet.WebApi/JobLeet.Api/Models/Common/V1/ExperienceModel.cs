8f5bb07bfcac55de1b81391449706d52dfe15a0e
Nishant Banjade <nishantbanjade07@gmail.com>
2024-03-09 17:47:41 +0545

    chore: update the Experience Model using DataAnnotations and serialization

--- JobLeet.WebApi/JobLeet.Api/Models/Common/V1/ExperienceModel.cs
+++ JobLeet.WebApi/JobLeet.Api/Models/Common/V1/ExperienceModel.cs
@@ -1,17 +1,28 @@
-﻿namespace JobLeet.WebApi.JobLeet.Api.Models.Common.V1
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace JobLeet.WebApi.JobLeet.Api.Models.Common.V1
 {
     public class ExperienceModel : BaseModel
     {
         public ExperienceLevel ExperienceLevel { get; set; }
     }
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum ExperienceLevel
     {
-        EntryLevel,
-        Intermediate,
-        Senior,
-        Lead,
-        Principal,
-        Executive,
-        Others
+        [Display(Name = "EntryLevel")]
+        EntryLevel = 1,
+        [Display(Name = "Intermediate")]
+        Intermediate = 2,
+        [Display(Name = "Senior")]
+        Senior = 3,
+        [Display(Name = "Lead")]
+        Lead = 4,
+        [Display(Name = "Principal")]
+        Principal = 5,
+        [Display(Name = "Executive")]
+        Executive = 6,
+        [Display(Name = "Others")]
+        Others = 7
     }
 }
