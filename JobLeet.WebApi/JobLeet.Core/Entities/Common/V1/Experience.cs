2cd3bdc7ecbb380cbc88524e2bda749860d1bc68
Nishant Banjade <nishantbanjade07@gmail.com>
2024-03-09 17:46:25 +0545

    chore: modify the existing Experience Entity using DataAnnotations and Serialization for better API readability

--- JobLeet.WebApi/JobLeet.Core/Entities/Common/V1/Experience.cs
+++ JobLeet.WebApi/JobLeet.Core/Entities/Common/V1/Experience.cs
@@ -1,17 +1,28 @@
-﻿namespace JobLeet.WebApi.JobLeet.Core.Entities.Common.V1
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace JobLeet.WebApi.JobLeet.Core.Entities.Common.V1
 {
     public class Experience : BaseEntity
     {
         public ExperienceLevel ExperienceLevel { get; set; }
     }
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum ExperienceLevel
     {
-        EntryLevel,
-        Intermediate,
-        Senior,
-        Lead,
-        Principal,
-        Executive,
-        Others
+        [Display(Name ="EntryLevel")]
+        EntryLevel = 1,
+        [Display(Name = "Intermediate")]
+        Intermediate = 2,
+        [Display(Name = "Senior")]
+        Senior = 3,
+        [Display(Name = "Lead")]
+        Lead = 4,
+        [Display(Name = "Principal")]
+        Principal = 5,
+        [Display(Name = "Executive")]
+        Executive = 6,
+        [Display(Name = "Others")]
+        Others = 7
     }
 }
