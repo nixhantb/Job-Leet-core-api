e5fcf6fad2d36339a16d9b70d51b370f110d194a
Nishant Banjade <nishantbanjade07@gmail.com>
2024-03-09 19:35:35 +0545

    add: Experience Controller class assigning route for experiences end point

---
+++ JobLeet.WebApi/JobLeet.Api/Controllers/Common/V1/ExperienceController.cs
@@ -0,0 +1,18 @@
+﻿using JobLeet.WebApi.JobLeet.Api.Logging;
+using JobLeet.WebApi.JobLeet.Api.Models.Common.V1;
+using JobLeet.WebApi.JobLeet.Core.Interfaces.Common.V1;
+using Microsoft.AspNetCore.Components;
+
+namespace JobLeet.WebApi.JobLeet.Api.Controllers.Common.V1
+{
+    [Route("api/v1/experience")]
+    public class ExperienceController : BaseApiController<ExperienceModel, IExperienceRepository>
+    {
+        public ExperienceController(IExperienceRepository experienceRepository, ILoggerManagerV1 logger)
+
+            : base(experienceRepository, logger)
+        {
+            
+        }
+    }
+}
