190d2f8130029211b98f0339ab061d1df7c79537
Nishant Banjade <nishantbanjade07@gmail.com>
2024-03-09 19:27:07 +0545

    implement experience configuration, implementing IEntityTypeConfiguration

---
+++ JobLeet.WebApi/JobLeet.Infrastructure/Data/Contexts/V1/ExperienceConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using JobLeet.WebApi.JobLeet.Core.Entities.Common.V1;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace JobLeet.WebApi.JobLeet.Infrastructure.Data.Contexts.V1
+{
+    public class ExperienceConfiguration : IEntityTypeConfiguration<Experience>
+    {
+        public void Configure(EntityTypeBuilder<Experience> builder)
+        {
+            builder.ToTable("Experience");
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Id).HasColumnName("experience_id");
+            builder.Property(e => e.ExperienceLevel).HasColumnName("experience_type");
+            builder.OwnsOne(experience => experience.MetaData);
+            
+        }
+    }
+}
