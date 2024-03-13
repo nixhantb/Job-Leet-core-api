6b217e95027eb7c5730e9ab280c8323ffadedef4
Nishant Banjade <nishantbanjade07@gmail.com>
2024-03-13 11:13:06 +0545

    Add ResourceNotFound Middleware Exception

---
+++ JobLeet.WebApi/JobLeet.Api/Exceptions/CustomExceptionWrappers/V1/ResourceNotFoundException.cs
@@ -0,0 +1,26 @@
+﻿namespace JobLeet.WebApi.JobLeet.Api.Exceptions.CustomExceptionWrappers.V1
+{
+    public class ResourceNotFoundException
+    {
+        private readonly RequestDelegate _next;
+
+        public ResourceNotFoundException(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke (HttpContext context)
+        {
+            await _next(context);
+
+            if(context.Response.StatusCode == StatusCodes.Status404NotFound  && !context.Response.HasStarted)
+            {
+                var requestPath = context.Request.Path;
+                var requestUri = $"{context.Request.Scheme}://{context.Request.Host}{requestPath}";
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = StatusCodes.Status404NotFound;
+                await context.Response.WriteAsync($"{{\"Message\": \"No HTTP resource was found that matches the request URI '{requestUri}'\"}}");
+            }
+        }
+    }
+}
