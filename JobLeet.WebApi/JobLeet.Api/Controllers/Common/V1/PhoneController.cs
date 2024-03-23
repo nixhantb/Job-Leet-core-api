1eb55a40e054b7f77d47b4c80f43faa19f1c2d07
Nishant Banjade <nishantbanjade07@gmail.com>
2024-03-23 07:13:30 +0545

    add phontController class

---
+++ JobLeet.WebApi/JobLeet.Api/Controllers/Common/V1/PhoneController.cs
@@ -0,0 +1,17 @@
+﻿using JobLeet.WebApi.JobLeet.Api.Logging;
+using JobLeet.WebApi.JobLeet.Api.Models.Common.V1;
+using JobLeet.WebApi.JobLeet.Core.Interfaces.Common.V1;
+using Microsoft.AspNetCore.Components;
+
+namespace JobLeet.WebApi.JobLeet.Api.Controllers.Common.V1
+{
+    [Route("api/v1/jobleet-phones")]
+    public class PhoneController : BaseApiController<PhoneModel, IPhoneRepository>
+    {
+        public PhoneController(IPhoneRepository phoneRepository, ILoggerManagerV1 logger) : base(phoneRepository, logger)
+        { 
+
+        }
+
+    }
+}
