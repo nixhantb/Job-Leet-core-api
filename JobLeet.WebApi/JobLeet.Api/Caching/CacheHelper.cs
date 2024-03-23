8c0af80a489be44c25ca5fbb6c16fc1765edb2e9
Nishant Banjade <nishantbanjade07@gmail.com>
2024-03-23 17:12:15 +0545

    add : CacheHelper wrapper

---
+++ JobLeet.WebApi/JobLeet.Api/Caching/CacheHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace JobLeet.WebApi.JobLeet.Api.Caching
+{
+    public static class CacheHelper
+    {
+       public static string CacheKey(string keyName)
+        {
+            return $"{keyName} _CacheKey";
+        }
+
+        public static MemoryCacheEntryOptions GetCacheOptions(TimeSpan expirationTime)
+        {
+            return new MemoryCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = expirationTime
+            };
+        }
+    }
+}
