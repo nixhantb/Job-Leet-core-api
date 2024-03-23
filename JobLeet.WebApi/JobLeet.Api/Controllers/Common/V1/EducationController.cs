6400a177aab12309e8925e114f6f720f506f64b6
Nishant Banjade <nishantbanjade07@gmail.com>
2024-03-23 22:13:28 +0545

    add: EducationController

---
+++ JobLeet.WebApi/JobLeet.Api/Controllers/Common/V1/EducationController.cs
@@ -0,0 +1,15 @@
+﻿using JobLeet.WebApi.JobLeet.Api.Logging;
+using JobLeet.WebApi.JobLeet.Api.Models.Common.V1;
+using JobLeet.WebApi.JobLeet.Core.Interfaces.Common.V1;
+using Microsoft.AspNetCore.Components;
+
+namespace JobLeet.WebApi.JobLeet.Api.Controllers.Common.V1
+{
+    [Route("api/v1/education")]
+    public class EducationController : BaseApiController<EducationModel, IEducationRepository>
+    {
+        public EducationController(IEducationRepository educationRepository, ILoggerManagerV1 logger)
+            :base(educationRepository, logger) { }
+        
+    }
+}
