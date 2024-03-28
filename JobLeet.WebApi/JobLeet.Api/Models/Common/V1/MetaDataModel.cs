5e2542ee434dc318ce8a3e71eaefebef6fa3c388
Nishant Banjade <nishantbanjade07@gmail.com>
2024-03-28 14:08:23 +0545

    feat: add Database Migrations for JobLeet

--- JobLeet.WebApi/JobLeet.Api/Models/Common/V1/MetaDataModel.cs
+++ JobLeet.WebApi/JobLeet.Api/Models/Common/V1/MetaDataModel.cs
@@ -1,8 +1,12 @@
-﻿namespace JobLeet.WebApi.JobLeet.Api.Models.Common.V1
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JobLeet.WebApi.JobLeet.Api.Models.Common.V1
 {
     public class MetaDataModel
     {
+        [NotMapped]
         public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+        [NotMapped]
         public DateTime? ModifiedOn { get; set; } = DateTime.UtcNow;
     }
 }
--- JobLeet.WebApi/JobLeet.Core/Entities/BaseEntity.cs
+++ JobLeet.WebApi/JobLeet.Core/Entities/BaseEntity.cs
@@ -1,9 +1,11 @@
 ﻿using JobLeet.WebApi.JobLeet.Core.Entities.Common.V1;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace JobLeet.WebApi.JobLeet.Core.Entities;
 public class BaseEntity
 {
     public int Id { get; set; }
+    [NotMapped]
     public MetaData MetaData { get; set; } = new MetaData();
 
 }
