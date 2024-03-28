04b5b271f93265ba8633292cac8330b7d851e409
Nishant Banjade <nishantbanjade07@gmail.com>
2024-03-28 22:54:10 +0545

    Clean up DB migrations

--- JobLeet.WebApi/JobLeet.Infrastructure/Data/Migrations/20240328170644_InitialCreate.Designer.cs
+++ JobLeet.WebApi/JobLeet.Infrastructure/Data/Migrations/20240328170644_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace JobLeet.WebApi.JobLeet.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(BaseDBContext))]
-    [Migration("20240328074154_InitialCreate")]
+    [Migration("20240328170644_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
