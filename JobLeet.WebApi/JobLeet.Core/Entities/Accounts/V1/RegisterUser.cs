6aedb3fc768e3614c30503915d5aa1a78a2da2a0
Nishant Banjade <nishantbanjade07@gmail.com>
2024-03-30 19:42:22 +0545

    add: Register User Entity

---
+++ JobLeet.WebApi/JobLeet.Core/Entities/Accounts/V1/RegisterUser.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobLeet.WebApi.JobLeet.Core.Entities.Accounts.V1
+{
+    public class RegisterUser : BaseEntity
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(17, ErrorMessage = "Password must be between 8 and 17 characters", MinimumLength = 8)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$",
+            ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character")]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+
+
+    }
+}
