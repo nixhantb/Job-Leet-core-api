afec75a73ea61f9cec82f58892a831f26b6e7f56
Nishant Banjade <nishantbanjade07@gmail.com>
2024-03-30 09:49:47 +0545

    Add: RoleEntity for the userRole which denotes the role for either admin or users

--- JobLeet.WebApi/JobLeet.Core/Entities/Accounts/V1/Role.cs
+++ JobLeet.WebApi/JobLeet.Core/Entities/Accounts/V1/Role.cs
@@ -1,13 +1,19 @@
-﻿namespace JobLeet.WebApi.JobLeet.Core.Entities.Accounts.V1
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace JobLeet.WebApi.JobLeet.Core.Entities.Accounts.V1
 {
     public class Role : BaseEntity
     {
         public RoleName RoleName { get; set; }
     }
 
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum RoleName
     {
-        Admin,
-        Users
+        [Display(Name ="Admin")]
+        Admin = 1,
+        [Display(Name ="Users")]
+        Users = 2
     }
 }
