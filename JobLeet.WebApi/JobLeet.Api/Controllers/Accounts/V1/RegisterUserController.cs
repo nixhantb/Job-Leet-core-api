06426677ed0a428f60b05f969253770b8a4d88a9
Nishant Banjade <nishantbanjade07@gmail.com>
2024-03-30 19:35:22 +0545

    add RegisterUserController for user registration

---
+++ JobLeet.WebApi/JobLeet.Api/Controllers/Accounts/V1/RegisterUserController.cs
@@ -0,0 +1,17 @@
+﻿using JobLeet.WebApi.JobLeet.Api.Logging;
+using JobLeet.WebApi.JobLeet.Api.Models.Accounts.V1;
+using JobLeet.WebApi.JobLeet.Core.Interfaces.Accounts.V1;
+using Microsoft.AspNetCore.Components;
+
+namespace JobLeet.WebApi.JobLeet.Api.Controllers.Accounts.V1
+{
+    [Route("api/v1/register")]
+    public class RegisterUserController : BaseApiController<RegisterUserModel, IRegisterUserRepository>
+    {
+        public RegisterUserController(IRegisterUserRepository registerUserRepository, ILoggerManagerV1 logger)
+            : base(registerUserRepository, logger)
+        {
+            
+        }
+    }
+}
