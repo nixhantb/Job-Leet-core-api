a4d555b1bc9754e409d7cf44a57ce89c1b31bee8
Nishant Banjade <nishantbanjade07@gmail.com>
2024-03-30 09:50:12 +0545

    add: IRoleRepository Interface for the Account

--- JobLeet.WebApi/JobLeet.Core/Interfaces/Accounts/V1/IRoleRepository.cs
+++ JobLeet.WebApi/JobLeet.Core/Interfaces/Accounts/V1/IRoleRepository.cs
@@ -1,6 +1,8 @@
-﻿namespace JobLeet.WebApi.JobLeet.Core.Interfaces.Accounts.V1
+﻿using JobLeet.WebApi.JobLeet.Api.Models.Accounts.V1;
+
+namespace JobLeet.WebApi.JobLeet.Core.Interfaces.Accounts.V1
 {
-    public interface IRoleRepository
+    public interface IRoleRepository : IRepository<RoleModel>
     {
     }
 }
