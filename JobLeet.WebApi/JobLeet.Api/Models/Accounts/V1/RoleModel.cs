bb6038e755712e8df33d130b2cca53e6a153baf6
Nishant Banjade <nishantbanjade07@gmail.com>
2024-03-30 09:48:58 +0545

    add: RoleModel for the userRole (Authentication)

---
+++ JobLeet.WebApi/JobLeet.Api/Models/Accounts/V1/RoleModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+namespace JobLeet.WebApi.JobLeet.Api.Models.Accounts.V1
+{
+    public class RoleModel : BaseModel
+    {
+        public RoleName RoleName { get; set; }
+    }
+
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum RoleName
+    {
+        [Display(Name = "Admin")]
+        Admin = 1,
+        [Display(Name = "Users")]
+        Users = 2
+    }
+
+}
+
