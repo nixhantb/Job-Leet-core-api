ff0a23f2993c5a267347aa781acbf4ddcab93469
Nishant Banjade <nishantbanjade07@gmail.com>
2024-03-30 19:42:47 +0545

    add: IRegisterUserRepository interface for the common crud operation

---
+++ JobLeet.WebApi/JobLeet.Core/Interfaces/Accounts/V1/IRegisterUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using JobLeet.WebApi.JobLeet.Api.Models.Accounts.V1;
+
+namespace JobLeet.WebApi.JobLeet.Core.Interfaces.Accounts.V1
+{
+    public interface IRegisterUserRepository : IRepository<RegisterUserModel>
+    {
+    }
+}
