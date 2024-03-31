1de4e8a3d7c7d56eb51b3a7b122be2b9c1930464
Nishant Banjade <nishantbanjade07@gmail.com>
2024-03-31 20:35:58 +0545

    add: Registration and Login repository

--- JobLeet.WebApi/JobLeet.Infrastructure/Repositories/Accounts/V1/RegisterUserRepository.cs
+++ JobLeet.WebApi/JobLeet.Infrastructure/Repositories/Accounts/V1/RegisterUserRepository.cs
@@ -2,6 +2,9 @@
 using JobLeet.WebApi.JobLeet.Core.Entities.Accounts.V1;
 using JobLeet.WebApi.JobLeet.Core.Interfaces.Accounts.V1;
 using JobLeet.WebApi.JobLeet.Infrastructure.Data.Contexts;
+using JobLeet.WebApi.JobLeet.Infrastructure.Repositories.Utilities;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace JobLeet.WebApi.JobLeet.Infrastructure.Repositories.Accounts.V1
 {
@@ -21,25 +24,54 @@ namespace JobLeet.WebApi.JobLeet.Infrastructure.Repositories.Accounts.V1
         {
             try
             {
+                // Check if the email address is already registered
+                bool emailExists = await _dbContext.RegisterUsers.AnyAsync(u => u.Email == entity.Email);
+                if (emailExists)
+                {
+                    throw new Exception("This email address is already registered.");
+                }
+
+                if (!PasswordValidation.ValidatePassword(entity.Password))
+                {
+                    throw new ArgumentException("Password must be between 8 and 100 characters and should match the regular expression pattern");
+                }
+
+                byte[] salt = GenerateHashedPassword.GenerateSalt();
+                string saltString = Convert.ToBase64String(salt);
+
+                // Generate hashed password using the salt
+                string hashedPasscode = GenerateHashedPassword.HashedPassword(entity.Password, saltString);
                 var newUser = new RegisterUser
                 {
                     UserName = entity.UserName,
                     Email = entity.Email,
-                    Password = entity.Password,
-                    ConfirmPassword = entity.ConfirmPassword,
+                    Password = hashedPasscode,
+                    ConfirmPassword = hashedPasscode,
+                    Salt = saltString 
                 };
 
                 _dbContext.RegisterUsers.Add(newUser);
                 await _dbContext.SaveChangesAsync();
-                entity.Id = newUser.Id; 
-                return entity;
-            }
-            catch(Exception ex)
-            {
-                throw new Exception($"Error occurred while Creating the user registration {ex.Message}");
-            }
-           
+                var responseEntity = new RegisterUserModel
+                {
+                    UserName = newUser.UserName,
+                    Email = newUser.Email,
+                    Password = hashedPasscode,
+                    ConfirmPassword = hashedPasscode,
+                    Id = newUser.Id,
+                };
 
+                return responseEntity;
+            }
+            catch (DbUpdateException ex)
+            {
+                throw new DbUpdateException($"Error occurred while Creating the user registration {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                // Catch any other unexpected exceptions and rethrow
+                throw new Exception($"An error occurred: {ex.Message}", ex);
+            }
         }
 
         public Task DeleteAsync(int id)
