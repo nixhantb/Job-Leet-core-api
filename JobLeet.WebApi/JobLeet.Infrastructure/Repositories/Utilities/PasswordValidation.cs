71fd14f3afdfccb7edfa3951f8dfc583b02ce755
Nishant Banjade <nishantbanjade07@gmail.com>
2024-03-31 20:35:33 +0545

    feat: Add Hashing function for password protection

---
+++ JobLeet.WebApi/JobLeet.Infrastructure/Repositories/Utilities/PasswordValidation.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace JobLeet.WebApi.JobLeet.Infrastructure.Repositories.Utilities
+{
+    public static class PasswordValidation
+    {
+        public static bool ValidatePassword(string password)
+        {
+            if (string.IsNullOrEmpty(password) || password.Length < 8 || password.Length > 101)
+            {
+                return false;
+            }
+            string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$";
+            if (!Regex.IsMatch(password, pattern))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
