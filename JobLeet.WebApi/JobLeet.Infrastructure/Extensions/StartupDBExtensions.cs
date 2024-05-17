013f2a54a31f1942402d18dda4a394e5c4e0d816
Nix-code <nishantbanjade07@gmail.com>
2024-05-17 19:18:01 +0545

    add: StartupDBExtension file to support database Migrations

---
+++ JobLeet.WebApi/JobLeet.Infrastructure/Extensions/StartupDBExtensions.cs
@@ -0,0 +1,31 @@
+
+using JobLeet.WebApi.JobLeet.Infrastructure.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobLeet.WebApi.JobLeet.Infrastructure.Extensions
+{
+
+    public static class StartupDBExtensions
+    {
+        public static async void CreateDataBaseTableIfNotPresent(this IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var dataBaseContext = services.GetRequiredService<BaseDBContext>();
+
+            var logger = services.GetRequiredService<ILogger<Program>>();
+
+            try
+            {
+                dataBaseContext.Database.EnsureCreated();
+                dataBaseContext.Database.Migrate();
+            }
+            catch (Exception ex)
+            {
+
+                logger.LogError($"There is something wrong with the Migration{ex.Message}");
+
+            }
+        }
+    }
+}
