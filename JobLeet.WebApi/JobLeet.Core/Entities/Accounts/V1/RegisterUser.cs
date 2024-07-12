52f6dd77b269ec41ae143aff12b9bde983d83a4e
Nix-code <nishantbanjade07@gmail.com>
2024-07-12 09:52:35 +0545

    update the register model and register entity

--- JobLeet.WebApi/JobLeet.Core/Entities/Accounts/V1/RegisterUser.cs
+++ JobLeet.WebApi/JobLeet.Core/Entities/Accounts/V1/RegisterUser.cs
@@ -1,13 +1,16 @@
 ﻿using JobLeet.WebApi.JobLeet.Core.Entities.Common.V1;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 namespace JobLeet.WebApi.JobLeet.Core.Entities.Accounts.V1
 {
     public class RegisterUser : BaseEntity
     {
+        [JsonIgnore]
         public string? Salt { get; set; }
         [Required(ErrorMessage = "Username is required")]
+        [RegularExpression(@"^[a-zA-Z0-9]+$", ErrorMessage = "Username contains invalid characters.")]
         public string UserName { get; set; }
-
+        [Required(ErrorMessage = "User email is required")]
         public Email UserEmail { get; set; }
         [Required(ErrorMessage = "Password is required")]
         [StringLength(101, ErrorMessage = "Password must be between 8 and 100 characters", MinimumLength = 8)]
