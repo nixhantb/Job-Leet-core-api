14b385cb286c3225978c64485442b6e0a76ea837
Nix-code <nishantbanjade07@gmail.com>
2024-07-21 05:16:56 +0545

    add: Repository file for Job Api

--- JobLeet.WebApi/JobLeet.Infrastructure/Repositories/Jobs/V1/JobRepository.cs
+++ JobLeet.WebApi/JobLeet.Infrastructure/Repositories/Jobs/V1/JobRepository.cs
@@ -1,3 +1,4 @@
+using JobLeet.WebApi.JobLeet.Api.Models.Companies.V1;
 using JobLeet.WebApi.JobLeet.Api.Models.Jobs.V1;
 using JobLeet.WebApi.JobLeet.Core.Entities.Jobs.V1;
 using JobLeet.WebApi.JobLeet.Core.Interfaces.Jobs.V1;
@@ -22,9 +23,37 @@ namespace JobLeet.WebApi.JobLeet.Infrastructure.Repositories.Jobs.V1
         {
             try
             {
-
+                #region  To JobLeet DB
                 var job = new JobEntity
                 {
+                    CompanyDescription = new(){
+                        
+                        CompanyName = entity.CompanyDescription.CompanyName,
+                        Profile = new(){
+                            ProfileInfo = entity.CompanyDescription.Profile.ProfileInfo,
+                            CompanyAddress = new(){
+                                // Headquater of the Company
+                                Street = entity.CompanyDescription.Profile.CompanyAddress.State,
+                                City = entity.CompanyDescription.Profile.CompanyAddress.City,
+                                State = entity.CompanyDescription.Profile.CompanyAddress.State,
+                                PostalCode = entity.CompanyDescription.Profile.CompanyAddress.PostalCode,
+                                Country = entity.CompanyDescription.Profile.CompanyAddress.Country
+                            },
+
+                            ContactPhone = new(){
+                                CountryCode = entity.CompanyDescription.Profile.ContactPhone.CountryCode,
+                                PhoneNumber = entity.CompanyDescription.Profile.ContactPhone.PhoneNumber
+                            },
+                            ContactEmail = new(){
+                                EmailType = entity.CompanyDescription.Profile.ContactEmail.EmailType,
+                                EmailAddress = entity.CompanyDescription.Profile.ContactEmail.EmailAddress
+                            },
+                            Website = entity.CompanyDescription.Profile.Website,
+                            IndustryType = entity.CompanyDescription.Profile.IndustryType
+                        }
+                    },
+                    JobTitle = entity.JobTitle,
+                    JobDescription = entity.JobDescription,
                     JobAddress = new()
                     {
                         Street = entity?.JobAddress?.Street,
@@ -39,58 +68,114 @@ namespace JobLeet.WebApi.JobLeet.Infrastructure.Repositories.Jobs.V1
                         QualificationType = entity.RequiredQualification.QualificationType,
                         QualificationInformation = entity.RequiredQualification.QualificationInformation
                     },
-                    JobTitle = entity.JobTitle,
-                    JobDescription = entity.JobDescription,
+                    JobType = entity.JobType,
                     Vacancies = entity.Vacancies,
+                    SkillsRequired = new () {
+                        Title = entity?.SkillsRequired?.Title,
+                        Description = entity?.SkillsRequired?.Description
+                    },
                     RequiredExperience = new()
                     {
                         ExperienceLevel = entity.RequiredExperience.ExperienceLevel
                     },
-                    BasicPay = entity.BasicPay,
-                    FunctionalArea = entity.FunctionalArea,
-                    IndustryType = new()
-                    {
-                        IndustryCategory = entity.IndustryType.IndustryCategory
+                    BasicPay = new(){
+                        MinmumPay = entity.BasicPay.MinmumPay,
+                        MaximumPay = entity.BasicPay.MaximumPay,
+                        Currency = entity.BasicPay.Currency
                     },
-                    PostingDate = entity.PostingDate
+                    PreferredQualifications = entity.PreferredQualifications,
+                    JobResponsibilities = entity.JobResponsibilities,
+                    Benefits = entity.Benefits,
+                    FunctionalArea = entity.FunctionalArea,
+                    WorkEnvironment = entity.WorkEnvironment,
+                    PostingDate = entity.PostingDate,
+                    ApplicationDeadline = entity.ApplicationDeadline
                 };
 
                 _dbContext.JobEntity.Add(job);
+                 await _dbContext.SaveChangesAsync();
+                #endregion
 
-                // Api response
+                #region  To Api Response
                 var jobResponse = new JobModel
                 {
                     Id = job.Id,
+                    JobTitle = job.JobTitle,
+                    JobDescription = job.JobDescription,
+                    JobType = job.JobType,
+                    CompanyDescription = new(){
+                        Id = job.CompanyDescription.Id,
+                        CompanyName = job.CompanyDescription.CompanyName,
+                        Profile = new(){
+                            ProfileInfo = entity.CompanyDescription.Profile.ProfileInfo,
+                            CompanyAddress = new(){
+                                // Headquater of the Company
+                                Id = job.CompanyDescription.Profile.CompanyAddress.Id,
+                                Street = job.CompanyDescription.Profile.CompanyAddress.State,
+                                City = job.CompanyDescription.Profile.CompanyAddress.City,
+                                State = job.CompanyDescription.Profile.CompanyAddress.State,
+                                PostalCode = job.CompanyDescription.Profile.CompanyAddress.PostalCode,
+                                Country = job.CompanyDescription.Profile.CompanyAddress.Country
+                            },
+
+                            ContactPhone = new(){
+                                CountryCode = job.CompanyDescription.Profile.ContactPhone.CountryCode,
+                                PhoneNumber = job.CompanyDescription.Profile.ContactPhone.PhoneNumber,
+                                Id = job.CompanyDescription.Profile.ContactPhone.Id
+                            },
+                            ContactEmail = new(){
+                                EmailType = (Api.Models.Common.V1.EmailCategory)job.CompanyDescription.Profile.ContactEmail.EmailType,
+                                EmailAddress = job.CompanyDescription.Profile.ContactEmail.EmailAddress,
+                                Id = job.CompanyDescription.Profile.ContactEmail.Id
+                            },
+                            Website = job.CompanyDescription.Profile.Website,
+                            IndustryType = (IndustryCategory)job.CompanyDescription.Profile.IndustryType,
+                            Id = job.CompanyDescription.Profile.Id
+                        }
+                    },
                     JobAddress = new()
+
                     {
+                        Id = job.JobAddress.Id,
                         Street = job.JobAddress.Street,
                         City = job.JobAddress.City,
                         State = job.JobAddress.State,
                         PostalCode = job.JobAddress.PostalCode,
                         Country = job.JobAddress.Country
                     },
+                    Vacancies = job.Vacancies,
+                    BasicPay = new(){
+                        MinmumPay  = job.BasicPay.MinmumPay,
+                        MaximumPay = job.BasicPay.MaximumPay,
+                        Currency = job.BasicPay.Currency
+                    },
+                    FunctionalArea = job.FunctionalArea,
+                    SkillsRequired = new(){
+                        Id = job.SkillsRequired.Id,
+                        Title = job.SkillsRequired.Title,
+                        Description = job.SkillsRequired.Description
+                    },
                     RequiredQualification = new()
                     {
+                        Id = job.RequiredQualification.Id,
                         QualificationType = (Api.Models.Common.V1.QualificationCategory)job.RequiredQualification.QualificationType,
-                        QualificationInformation = job.RequiredQualification.QualificationInformation
+                        QualificateionInformation= job.RequiredQualification.QualificationInformation
                     },
-                    JobTitle = job.JobTitle,
-                    JobDescription = job.JobDescription,
-                    Vacancies = job.Vacancies,
-
+                    
                     RequiredExperience = new()
                     {
+                        Id = job.RequiredExperience.Id,
                         ExperienceLevel = (Api.Models.Common.V1.ExperienceLevel)job.RequiredExperience.ExperienceLevel
                     },
-                    BasicPay = job.BasicPay,
-                    FunctionalArea = job.FunctionalArea,
-                    IndustryType = new()
-                    {
-                        IndustryCategory = (Api.Models.Companies.V1.IndustryCategory)job.IndustryType.IndustryCategory
-                    },
-                    PostingDate = job.PostingDate
 
+                    PreferredQualifications = job.PreferredQualifications,
+                    JobResponsibilities = job.JobResponsibilities,
+                    Benefits = job.Benefits,
+                    WorkEnvironment = job.WorkEnvironment,
+                    PostingDate = job.PostingDate,
+                    ApplicationDeadline = job.PostingDate
                 };
+                #endregion
                 return jobResponse;
 
             }
