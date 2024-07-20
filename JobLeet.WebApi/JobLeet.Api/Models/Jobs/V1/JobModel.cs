521c7ceedf90bb979adfc396d591a3bc3027a3fd
Nix-code <nishantbanjade07@gmail.com>
2024-07-21 05:16:33 +0545

    add: job Model with nested entities

--- JobLeet.WebApi/JobLeet.Api/Models/Jobs/V1/JobModel.cs
+++ JobLeet.WebApi/JobLeet.Api/Models/Jobs/V1/JobModel.cs
@@ -1,22 +1,27 @@
 ﻿using JobLeet.WebApi.JobLeet.Api.Models.Common.V1;
 using JobLeet.WebApi.JobLeet.Api.Models.Companies.V1;
-using JobLeet.WebApi.JobLeet.Api.Models.Employers.V1;
+using JobLeet.WebApi.JobLeet.Core.Entities.Common.V1;
 
 namespace JobLeet.WebApi.JobLeet.Api.Models.Jobs.V1
 {
     public class JobModel : BaseModel
     {
-        public AddressModel? JobAddress { get; set; }
-        public QualificationModel? RequiredQualification { get; set; }
+        public CompanyModel? CompanyDescription {get; set;}
         public string? JobTitle { get; set; }
         public string? JobDescription { get; set; }
+        public string? JobType { get; set; }
+        public AddressModel? JobAddress { get; set; }
         public int? Vacancies { get; set; }
-        public ExperienceModel? RequiredExperience { get; set; }
-
-        public decimal? BasicPay { get; set; }
+        public BasicPayModel? BasicPay {get; set;}
         public string? FunctionalArea { get; set; }
-        public IndustryTypeModel? IndustryType { get; set; }
-
+        public SkillModel? SkillsRequired { get; set; }
+        public QualificationModel? RequiredQualification { get; set; }
+        public ExperienceModel? RequiredExperience { get; set; }
+        public List<string>? PreferredQualifications { get; set; }
+        public List<string>? JobResponsibilities { get; set; }
+        public List<string>? Benefits { get; set; }
+        public string? WorkEnvironment { get; set; }
         public DateTime? PostingDate { get; set; }
+        public DateTime? ApplicationDeadline { get; set; }
     }
 }
