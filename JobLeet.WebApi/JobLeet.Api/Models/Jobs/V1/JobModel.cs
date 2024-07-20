5b156347006249f5886e1531adcfa60a8f28d0da
Nix-code <nishantbanjade07@gmail.com>
2024-07-20 10:13:03 +0545

    add: job model for the job api

--- JobLeet.WebApi/JobLeet.Api/Models/Jobs/V1/JobModel.cs
+++ JobLeet.WebApi/JobLeet.Api/Models/Jobs/V1/JobModel.cs
@@ -6,17 +6,17 @@ namespace JobLeet.WebApi.JobLeet.Api.Models.Jobs.V1
 {
     public class JobModel : BaseModel
     {
-        public AddressModel JobAddress { get; set; }
-        public QualificationModel RequiredQualification { get; set; }
-        public string JobTitle { get; set; }
-        public string JobDescription { get; set; }
-        public int Vacancies { get; set; }
-        public ExperienceModel RequiredExperience { get; set; }
+        public AddressModel? JobAddress { get; set; }
+        public QualificationModel? RequiredQualification { get; set; }
+        public string? JobTitle { get; set; }
+        public string? JobDescription { get; set; }
+        public int? Vacancies { get; set; }
+        public ExperienceModel? RequiredExperience { get; set; }
 
-        public decimal BasicPay { get; set; }
-        public string FunctionalArea { get; set; }
-        public IndustryTypeModel IndustryType { get; set; }
+        public decimal? BasicPay { get; set; }
+        public string? FunctionalArea { get; set; }
+        public IndustryTypeModel? IndustryType { get; set; }
 
-        public DateTime PostingDate { get; set; }
+        public DateTime? PostingDate { get; set; }
     }
 }
--- JobLeet.WebApi/JobLeet.Core/Entities/Jobs/V1/Job.cs
+++ JobLeet.WebApi/JobLeet.Core/Entities/Jobs/V1/Job.cs
@@ -5,24 +5,18 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace JobLeet.WebApi.JobLeet.Core.Entities.Jobs.V1
 {
-    public class Job : BaseEntity
+    public class JobEntity : BaseEntity
     {
-        [ForeignKey("Employer")]
-        public Employer Employer { get; set; }
-        [ForeignKey("Address")]
-        public Address JobAddress { get; set; }
-        [ForeignKey("Qualification")]
-        public Qualification RequiredQualification { get; set; }
-        public string JobTitle { get; set; }
-        public string JobDescription { get; set; }
-        public int Vacancies { get; set; }
-        [ForeignKey("Experience")]
-        public Experience RequiredExperience { get; set; }
-        public decimal BasicPay { get; set; }
-        public string FunctionalArea { get; set; }
-        [ForeignKey("IndustryType")]
-        public IndustryType IndustryType { get; set; }
+        public Address? JobAddress { get; set; }
+        public Qualification? RequiredQualification { get; set; }
+        public string? JobTitle { get; set; }
+        public string? JobDescription { get; set; }
+        public int? Vacancies { get; set; }
+        public Experience? RequiredExperience { get; set; }
+        public decimal? BasicPay { get; set; }
+        public string? FunctionalArea { get; set; }
+        public IndustryType? IndustryType { get; set; }
 
-        public DateTime PostingDate { get; set; }
+        public DateTime? PostingDate { get; set; }
     }
 }
