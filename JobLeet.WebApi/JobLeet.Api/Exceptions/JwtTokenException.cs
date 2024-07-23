bdee8825e4acbf23ea25aa989666b60056ca6a40
Nix-code <nishantbanjade07@gmail.com>
2024-07-24 01:30:03 +0545

    add: Base exception class for JWT #60

---
+++ JobLeet.WebApi/JobLeet.Api/Exceptions/JwtTokenException.cs
@@ -0,0 +1,13 @@
+namespace JobLeet.WebApi.JobLeet.Api.Exceptions{
+
+    public class JwtTokenException: Exception{
+
+        public JwtTokenException(string message): base(message){
+
+        }
+
+        public JwtTokenException(string message, Exception innerException): base(message, innerException){
+
+        }
+    }
+}
