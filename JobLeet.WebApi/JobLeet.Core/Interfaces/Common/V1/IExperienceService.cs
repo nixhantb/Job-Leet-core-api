550b2f3c9d4bce1e46b82361d3d2716644912fd4
Nix-code <nishantbanjade07@gmail.com>
2025-01-17 21:56:30 +0545

    add Service layer Interfaces for handling complex business logics

---
+++ JobLeet.WebApi/JobLeet.Core/Interfaces/Common/V1/IExperienceService.cs
@@ -0,0 +1,9 @@
+using JobLeet.WebApi.JobLeet.Api.Models.Common.V1;
+using JobLeet.WebApi.JobLeet.Core.Entities.Common.V1;
+
+namespace JobLeet.WebApi.JobLeet.Core.Interfaces.Common.V1
+{
+    public interface IExperienceService : IService<Experience,ExperienceModel>
+    {
+    }
+}
