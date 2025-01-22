46a35bb16927c00e43c5e5593857b3df497601aa
Nix-code <nishantbanjade07@gmail.com>
2025-01-22 23:00:27 +0545

    clean: BaseEntity and add payload for seekers

--- Server/JobLeet.WebApi/JobLeet.Core/Entities/BaseEntity.cs
+++ Server/JobLeet.WebApi/JobLeet.Core/Entities/BaseEntity.cs
@@ -1,13 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace JobLeet.WebApi.JobLeet.Core.Entities;
 
 public class BaseEntity
 {
+    [JsonIgnore]
     public int Id { get; set; }
 
     [Required]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [JsonIgnore]
     public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
 }
